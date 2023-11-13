b1afa6076f2945cd2de20b70dd1d9521db4a0401
Mich <mitchellefetty@gmail.com>
2023-11-13 10:38:35 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by MIch on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
