eac8c3a5c45529c4a541bc4f233795816600fccc
Mich <mitchellefetty@gmail.com>
2023-11-13 11:43:56 +0200

    background added

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.blue, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+       
         }
-        .padding()
+        
     }
 }
 
