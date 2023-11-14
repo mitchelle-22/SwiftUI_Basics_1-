9fa9b12f50339a87efddb4b3db84b6e05db1d5b5
Mich <mitchellefetty@gmail.com>
2023-11-14 11:57:38 +0200

    refactor

---
+++ Weather/ButtonView.swift
@@ -0,0 +1,22 @@
+//
+//  ButtonView.swift
+//  Weather
+//
+//  Created by MIch on 2023/11/14.
+//
+
+import SwiftUI
+
+struct ButtonView : View {
+    var name : String
+    var textColor: Color
+    var backgroundColor: Color
+    var body: some View {
+        Text(name)
+            .frame(width: 280,height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20,weight: .bold,design: .default))
+            .cornerRadius(10)
+    }
+}
--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -112,16 +112,4 @@ struct ImageView:View {
        
 }
    
-struct ButtonView : View {
-    var name : String
-    var textColor: Color
-    var backgroundColor: Color
-    var body: some View {
-        Text(name)
-            .frame(width: 280,height: 50)
-            .background(backgroundColor)
-            .foregroundColor(textColor)
-            .font(.system(size: 20,weight: .bold,design: .default))
-            .cornerRadius(10)
-    }
-}
+
